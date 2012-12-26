7abd04a6572ae2bad9859593d5619029fea1f452
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-12-26 17:02:35 -0500

    Add reference to mdpi-common resources
    Change-Id: I08c3052370283aab96cb7f689a58fd9c94b07637

--- p506.mk
+++ p506.mk
@@ -5,6 +5,7 @@ $(call inherit-product, device/lge/msm7x27-common/device.mk)
 $(call inherit-product, vendor/lge/p506/p506-vendor.mk)
 $(call inherit-product, vendor/lge/msm7x27-common/msm7x27-common-vendor-blobs.mk)
 
+$(call inherit-product, device/mdpi-common/mdpi.mk)
 DEVICE_PACKAGE_OVERLAYS += device/lge/p506/overlay
 
 # p506 configs
