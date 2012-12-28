83c712a04e0fedc5833eef483c9c72649f8fc807
AndroidMeda <hephappy@gmail.com>
2012-12-28 15:20:48 -0500

    Fix bluetooth 1/2 (WIP)
    Change-Id: I6e578ac7b83a9e49bbad27265b79608a83a85770

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 TARGET_KERNEL_CONFIG := cyanogenmod_p505_p506_defconfig
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p506 no_console_suspend
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p506/bluetooth
-
 TARGET_BOOTLOADER_BOARD_NAME := p506
 TARGET_OTA_ASSERT_DEVICE := thunderg,p506
+
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p506/bluetooth
--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "P506"
+#define BTM_DEF_LOCAL_NAME   "LGE-P506"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #endif
