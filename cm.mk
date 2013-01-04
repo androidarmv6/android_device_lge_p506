747e6612c6fd4131cb1272e2c836b3e49a6a1f84
AndroidMeda <hephappy@gmail.com>
2013-01-03 19:57:30 -0500

    p506: Use tiny config
    Change-Id: I1c903af73805fa10da3aad8749ccb3f75e563ecb

--- cm.mk
+++ cm.mk
@@ -1,19 +1,14 @@
-# Boot Animation
-TARGET_SCREEN_HEIGHT := 480
-TARGET_SCREEN_WIDTH := 320
-
 # Include GSM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit device configuration for p506.
 $(call inherit-product, device/lge/p506/p506.mk)
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+$(call inherit-product, vendor/cm/config/tiny.mk)
 
-# Include FM-Radio stuff
-#$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
-
+# Overrides
 PRODUCT_NAME := cm_p506
 PRODUCT_BRAND := lge
 PRODUCT_DEVICE := p506
@@ -21,12 +16,11 @@ PRODUCT_MODEL := LG-P506
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_CHARACTERISTICS := phone
 
+# Release name and versioning
+PRODUCT_RELEASE_NAME := OptimusThrive
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lge_p506 \
     BUILD_FINGERPRINT="lge/thunderg/thunderg:2.3.3/GRJ22/LG-P506-V11f.19E55B0D28:user/release-keys" \
     PRIVATE_BUILD_DESC="thunderg-user 2.3.3 GRJ22 LG-P506-V11f.19E55B0D28 release-keys"
-
-# Release name and versioning
-PRODUCT_RELEASE_NAME := OptimusThrive
-PRODUCT_VERSION_DEVICE_SPECIFIC :=
--include vendor/cyanogen/products/common_versions.mk
